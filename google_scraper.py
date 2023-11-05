49ae177d5860a4b16696155db09ea23c1180d1af
Austin Berry <aberry52098@gmail.com>
2023-11-05 06:17:42 -0500

    use only get_original_images

--- google_scraper.py
+++ google_scraper.py
@@ -16,46 +16,6 @@ params = {
 html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
 soup = BeautifulSoup(html.text, "lxml")
 
-def get_images_with_request_headers():
-    del params["ijn"]
-    params["content-type"] = "image/png" # parameter that indicate the original media type
-    image = [img['src'] for img in soup.select("img")]
-    print(image)
-    return image
-get_images_with_request_headers()
-def get_suggested_search_data():
-    suggested_searches = []
-
-    all_script_tags = soup.select("script")
-
-    # https://regex101.com/r/48UZhY/6
-    matched_images = "".join(re.findall(r"AF_initDataCallback\(({key: 'ds:1'.*?)\);</script>", str(all_script_tags)))
-    
-    # https://kodlogs.com/34776/json-decoder-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
-    # if you try to json.loads() without json.dumps it will throw an error:
-    # "Expecting property name enclosed in double quotes"
-    matched_images_data_fix = json.dumps(matched_images)
-    matched_images_data_json = json.loads(matched_images_data_fix)
-
-    # search for only suggested search thumbnails related
-    # https://regex101.com/r/ITluak/2
-    suggested_search_thumbnails = ",".join(re.findall(r'{key(.*?)\[null,\"Size\"', matched_images_data_json))
-
-    # https://regex101.com/r/MyNLUk/1
-    suggested_search_thumbnail_encoded = re.findall(r'\"(https:\/\/encrypted.*?)\"', suggested_search_thumbnails)
-
-    for suggested_search, suggested_search_fixed_thumbnail in zip(soup.select(".PKhmud.sc-it.tzVsfd"), suggested_search_thumbnail_encoded):
-        suggested_searches.append({
-            "name": suggested_search.select_one(".VlHyHc").text,
-            "link": f"https://www.google.com{suggested_search.a['href']}",
-            # https://regex101.com/r/y51ZoC/1
-            "chips": "".join(re.findall(r"&chips=(.*?)&", suggested_search.a["href"])),
-            # https://stackoverflow.com/a/4004439/15164646 comment by Frédéric Hamidi
-            "thumbnail": bytes(suggested_search_fixed_thumbnail, "ascii").decode("unicode-escape")
-        })
-
-    return suggested_searches
-
 def get_original_images():
 
     """
@@ -67,7 +27,7 @@ def get_original_images():
     google_images = []
 
     all_script_tags = soup.select("script")
-
+    
     # # https://regex101.com/r/48UZhY/4
     matched_images_data = "".join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
     
@@ -81,7 +41,7 @@ def get_original_images():
     matched_google_images_thumbnails = ", ".join(
         re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
                    str(matched_google_image_data))).split(", ")
-
+    
     thumbnails = [
         bytes(bytes(thumbnail, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for thumbnail in matched_google_images_thumbnails
     ]
@@ -97,24 +57,24 @@ def get_original_images():
     full_res_images = [
         bytes(bytes(img, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for img in matched_google_full_resolution_images
     ]
-    
-    for index, (metadata, thumbnail, original) in enumerate(zip(soup.select('.isv-r.PNCib.MSM1fd.BUooTd'), thumbnails, full_res_images), start=1):
+
+    for thumbnail, original in zip(thumbnails, full_res_images):
         google_images.append({
-            "title": metadata.select_one(".VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb")["title"],
-            "link": metadata.select_one(".VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb")["href"],
-            "source": metadata.select_one(".fxgdke").text,
             "thumbnail": thumbnail,
             "original": original
         })
 
+
         # Download original images
-        print(f'Downloading {index} image...')
+        # print(f'Downloading {index} image...')
         
-        opener=urllib.request.build_opener()
-        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36')]
-        urllib.request.install_opener(opener)
+        # opener=urllib.request.build_opener()
+        # opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36')]
+        # urllib.request.install_opener(opener)
 
-        urllib.request.urlretrieve(original, f'Bs4_Images/original_size_img_{index}.jpg')
-    
+        # urllib.request.urlretrieve(original, f'Bs4_Images/original_size_img_{index}.jpg')
+    print("google images: ", google_images[0])
     return google_images
-
+# get_images_with_request_headers()
+# get_suggested_search_data()
+get_original_images()
