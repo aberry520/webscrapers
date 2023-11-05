7d89e30246131be10937e32e3767c41e6e42fb1b
Austin Berry <aberry52098@gmail.com>
2023-11-05 06:46:53 -0500

    fixed site parameter

--- google_scraper.py
+++ google_scraper.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 }
-search = str(input("search: "))
+search = "site:cigarpage.com "+input("search: ")
 params = {
-    "q": search, # search query
+    "q": search,                  # search query
     "tbm": "isch",                # image results
     "hl": "en",                   # language of the search
     "gl": "us",                   # country where search comes from
@@ -15,29 +15,19 @@ params = {
 
 html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
 soup = BeautifulSoup(html.text, "lxml")
-
+print(html.url)
 def get_original_images():
-
-    """
-    https://kodlogs.com/34776/json-decoder-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
-    if you try to json.loads() without json.dumps() it will throw an error:
-    "Expecting property name enclosed in double quotes"
-    """
-
     google_images = []
 
     all_script_tags = soup.select("script")
     
-    # # https://regex101.com/r/48UZhY/4
     matched_images_data = "".join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
     
     matched_images_data_fix = json.dumps(matched_images_data)
     matched_images_data_json = json.loads(matched_images_data_fix)
 
-    # https://regex101.com/r/VPz7f2/1
     matched_google_image_data = re.findall(r'\"b-GRID_STATE0\"(.*)sideChannel:\s?{}}', matched_images_data_json)
 
-    # https://regex101.com/r/NnRg27/1
     matched_google_images_thumbnails = ", ".join(
         re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
                    str(matched_google_image_data))).split(", ")
@@ -73,8 +63,7 @@ def get_original_images():
         # urllib.request.install_opener(opener)
 
         # urllib.request.urlretrieve(original, f'Bs4_Images/original_size_img_{index}.jpg')
-    print("google images: ", google_images[0])
+    print(google_images[0])
     return google_images
-# get_images_with_request_headers()
-# get_suggested_search_data()
+
 get_original_images()
