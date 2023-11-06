5449c19967ee6900e0305f43eb0a21fb627d2e36
Austin Berry <aberry52098@gmail.com>
2023-11-05 23:26:48 -0500

    try get_images 5 times if return null

--- google_scraper.py
+++ google_scraper.py
@@ -32,12 +32,15 @@ def connectDB():
     conn.close()
     print("disconnected")
 
+
 def get_images(search_name):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-    }
-    search = """
-    site:cigarpage.com OR 
+    count = 1
+    while (True):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+        }
+        search = """
+        site:cigarpage.com OR 
 site:cigarsdaily.com OR 
 site:cigarsinternational.com OR 
 site:foxcigar.com OR 
@@ -47,79 +50,90 @@ site:cigargod.com OR
 site:thecigarthief.com OR 
 site:cigarandpipes.com OR 
 site:coronacigar.com OR 
-site:neptunecigar.com
+site:neptunecigar.com OR 
+site:cubadoro.ch 
 """+search_name
 
-    params = {
-        "q": search,                  # search query
-        "tbm": "isch",                # image results
-        "hl": "en",                   # language of the search
-        "gl": "us",                   # country where search comes from
-        "ijn": "0"                    # page number
-    }
+        params = {
+            "q": search,                  # search query
+            "tbm": "isch",                # image results
+            "hl": "en",                   # language of the search
+            "gl": "us",                   # country where search comes from
+            "ijn": "0"                    # page number
+        }
 
-    html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
-    soup = BeautifulSoup(html.text, "lxml")
-    # print(html.url)
+        html = requests.get("https://www.google.com/search", params=params, headers=headers, timeout=30)
+        soup = BeautifulSoup(html.text, "lxml")
+        # print(html.url)
 
-    google_images = []
+        google_images = []
 
-    all_script_tags = soup.select("script")
-    
-    matched_images_data = "".join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
-    
-    matched_images_data_fix = json.dumps(matched_images_data)
-    matched_images_data_json = json.loads(matched_images_data_fix)
-    
-    matched_google_image_data = re.findall(r'\"b-GRID_STATE0\"(.*)sideChannel:\s?{}}', matched_images_data_json)
-
-    matched_google_images_thumbnails = ", ".join(
-        re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
-                   str(matched_google_image_data))).split(", ")
-    
-    thumbnails = [
-        bytes(bytes(thumbnail, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for thumbnail in matched_google_images_thumbnails
-    ]
-
-    # removing previously matched thumbnails for easier full resolution image matches.
-    removed_matched_google_images_thumbnails = re.sub(
-        r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]', "", str(matched_google_image_data))
-
-    # https://regex101.com/r/fXjfb1/4
-    # https://stackoverflow.com/a/19821774/15164646
-    matched_google_full_resolution_images = re.findall(r"(?:'|,),\[\"(https:|http.*?)\",\d+,\d+\]", removed_matched_google_images_thumbnails)
-
-    full_res_images = [
-        bytes(bytes(img, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for img in matched_google_full_resolution_images
-    ]
-
-    for thumbnail, original in zip(thumbnails, full_res_images):
-        google_images.append({
-            "thumbnail": thumbnail,
-            "image": original
-        })
-
-
-        # Download original images
-        # print(f'Downloading {index} image...')
+        all_script_tags = soup.select("script")
         
-        # opener=urllib.request.build_opener()
-        # opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36')]
-        # urllib.request.install_opener(opener)
+        matched_images_data = "".join(re.findall(r"AF_initDataCallback\(([^<]+)\);", str(all_script_tags)))
+        
+        matched_images_data_fix = json.dumps(matched_images_data)
+        matched_images_data_json = json.loads(matched_images_data_fix)
+        
+        matched_google_image_data = re.findall(r'\"b-GRID_STATE0\"(.*)sideChannel:\s?{}}', matched_images_data_json)
 
-        # urllib.request.urlretrieve(original, f'Bs4_Images/original_size_img_{index}.jpg')
-    # print(google_images[0])
-    # print(thumbnails)
-    if len(google_images) > 0:
-        return google_images[0]
-    else:
-        return None
+        matched_google_images_thumbnails = ", ".join(
+            re.findall(r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]',
+                    str(matched_google_image_data))).split(", ")
+        
+        thumbnails = [
+            bytes(bytes(thumbnail, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for thumbnail in matched_google_images_thumbnails
+        ]
+
+        # removing previously matched thumbnails for easier full resolution image matches.
+        removed_matched_google_images_thumbnails = re.sub(
+            r'\[\"(https\:\/\/encrypted-tbn0\.gstatic\.com\/images\?.*?)\",\d+,\d+\]', "", str(matched_google_image_data))
+
+        # https://regex101.com/r/fXjfb1/4
+        # https://stackoverflow.com/a/19821774/15164646
+        matched_google_full_resolution_images = re.findall(r"(?:'|,),\[\"(https:|http.*?)\",\d+,\d+\]", removed_matched_google_images_thumbnails)
+
+        full_res_images = [
+            bytes(bytes(img, "ascii").decode("unicode-escape"), "ascii").decode("unicode-escape") for img in matched_google_full_resolution_images
+        ]
+
+        for thumbnail, original in zip(thumbnails, full_res_images):
+            google_images.append({
+                "thumbnail": thumbnail,
+                "image": original
+            })
+
+
+            # Download original images
+            # print(f'Downloading {index} image...')
+            
+            # opener=urllib.request.build_opener()
+            # opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36')]
+            # urllib.request.install_opener(opener)
+
+            # urllib.request.urlretrieve(original, f'Bs4_Images/original_size_img_{index}.jpg')
+        # print(google_images[0])
+        # print(thumbnails)
+        
+        if len(google_images) > 0:
+            return google_images[0]
+        else:
+            print("Try again "+ search_name)
+            print(count)
+            if count < 5:
+                count = count+1
+            else:
+                None
+                break
+
+            
 
 
 def get_images_by_name():
     images_dictionary=[]
     for name in get_names():
         images_dictionary.append({"name":name, "images":get_images(name)})
+        print(name+" images retrieved")
         
     images_dictionary_string = json.dumps(images_dictionary, indent=4)
 
@@ -147,3 +161,4 @@ def get_images_by_name():
     
 get_images_by_name()
 print("file created")
+# get_images("13 Cigars Torpedo")
